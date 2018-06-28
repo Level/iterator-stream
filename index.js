552dbb45d21331ed03e0a6797dbf017501f6cff4
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-06-28 18:52:50 +0200

    Revert "Proper destroy (#34)"
    This reverts commit 9460d1b29e5867f28804b741b7c504d1a9091618.

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ function ReadStream (iterator, options) {
     objectMode: true
   }))
   this._iterator = iterator
+  this._destroyed = false
   this._options = options
-  this.on('end', this.destroy.bind(this, null, null))
+  this.on('end', this._cleanup.bind(this))
 }
 
 ReadStream.prototype._read = function () {
   var self = this
   var options = this._options
-  if (this.destroyed) return
+  if (this._destroyed) return
 
   this._iterator.next(function (err, key, value) {
-    if (self.destroyed) return
-    if (err) return self.destroy(err)
-
+    if (self._destroyed) return
+    if (err) return self.emit('error', err)
     if (key === undefined && value === undefined) {
       self.push(null)
     } else if (options.keys !== false && options.values === false) {
@@ -37,16 +37,14 @@ ReadStream.prototype._read = function () {
   })
 }
 
-ReadStream.prototype._destroy = function (err, callback) {
+ReadStream.prototype.destroy =
+ReadStream.prototype._cleanup = function () {
   var self = this
+  if (this._destroyed) return
+  this._destroyed = true
 
-  this._iterator.end(function (err2) {
-    callback(err || err2)
-
-    // TODO when the next readable-stream (mirroring node v10) is out:
-    // remove this. Since nodejs/node#19836, streams always emit close.
-    process.nextTick(function () {
-      self.emit('close')
-    })
+  this._iterator.end(function (err) {
+    if (err) return self.emit('error', err)
+    self.emit('close')
   })
 }
